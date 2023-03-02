b9daa2206603c030f652707dab17fc150ab990ed
RobertAvram <robert.avram@caseware.com>
2023-03-02 15:05:51 +0200

    Added storybook project with task list story.

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  stories: [
+    '../src/app/components/**/*.stories.ts'
+  ],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: "@storybook/angular",
+  core: {
+    builder: "@storybook/builder-webpack5",
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+};
---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,43 @@
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+})
+export class TaskListComponent {
+  /**
+  * @ignore
+  * Component property to define ordering of tasks
+  */
+  tasksInOrder: Task[] = [];
+
+  @Input()
+  set tasks(arr: Task[]) {
+    const initialTasks = [
+      ...arr.filter(t => t.state === 'TASK_PINNED'),
+      ...arr.filter(t => t.state !== 'TASK_PINNED'),
+    ];
+    const filteredTasks = initialTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+    this.tasksInOrder = filteredTasks.filter(
+      t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+    );
+  }
+
+  /** Checks if it's in loading state */
+  @Input() loading = false;
+
+  /** Event to change the task to pinned */
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  /** Event to change the task to archived */
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+}
